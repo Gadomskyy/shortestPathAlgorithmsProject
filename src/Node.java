5dcfccd6c408ad977f7ed271d00fdcb721cb4269
Gadomskyy <118121980+Gadomskyy@users.noreply.github.com>
2025-11-19 19:00:58 +0100

    add Node class

---
+++ src/Node.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Node {
+    private final String id;
+    private final List<Edge> edges;
+
+    public Node(String id) {
+        this.id = id;
+        this.edges = new ArrayList<>();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public List<Edge> getEdges() {
+        return edges;
+    }
+
+    public void addEdge(Edge edge) {
+        edges.add(edge);
+    }
+
+    @Override
+    public String toString() {
+        return id;
+    }
+}
