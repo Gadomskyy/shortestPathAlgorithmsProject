25ce956ce25452d04259ed98494a8b93d4d0536c
Gadomskyy <118121980+Gadomskyy@users.noreply.github.com>
2025-11-28 11:01:46 +0100

    Add BFS shortest path algorithm to Graph class

--- src/graphs/Graph.java
+++ src/graphs/Graph.java
@@ -1,7 +1,6 @@
 package graphs;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Graph {
     private final List<Node> nodes;
@@ -39,4 +38,50 @@ public class Graph {
             System.out.println();
         }
     }
+
+    public List<Node> BFSShortestPath(Node start, Node goal) {
+
+        Queue<Node> queue = new LinkedList<>();
+        Set<Node> visited = new HashSet<>();
+        Map<Node, Node> parent = new HashMap<>();
+
+        queue.add(start);
+        visited.add(start);
+        parent.put(start, null);
+
+        while (!queue.isEmpty()) {
+            Node current = queue.poll();
+
+            if (current.equals(goal)) {
+                List<Node> path = new ArrayList<>();
+                for (Node n = goal; n != null; n = parent.get(n)) {
+                    path.add(n);
+                }
+                Collections.reverse(path);
+
+                System.out.print("Shortest path: ");
+                for (int i = 0; i < path.size(); i++) {
+                    System.out.print(path.get(i));
+                    if (i < path.size() - 1) System.out.print(" -> ");
+                }
+                System.out.println();
+
+                return path;
+            }
+
+            for (Edge edge : current.getEdges()) {
+                Node neighbor = edge.getTo();
+                if (!visited.contains(neighbor)) {
+                    visited.add(neighbor);
+                    parent.put(neighbor, current);
+                    queue.add(neighbor);
+                }
+            }
+        }
+
+        //if no path is found
+        System.out.println("No path exists between " + start + " and " + goal);
+        return null;
+    }
+
 }
